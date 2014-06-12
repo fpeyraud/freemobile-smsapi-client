28fd0694b041b46866a6466c33ab40d5e93d28a4
Damien Nadé <github@livna.org>
2014-06-12 09:54:45 +0200

    Lignes coupees a 80 caracteres pour une meilleure lisibilite. Utilisation inutile de echo/sed. Curl peut faire son propre url-encodage

--- send-notification.sh
+++ send-notification.sh
@@ -18,49 +18,49 @@
 ## Configuration système
 ##
 
-# Caractère de fin de ligne (http://en.wikipedia.org/wiki/Percent-encoding#Character_data)
-NEWLINE_CHAR="%0D" # Valeurs possibles : %0A, %0D et %0D%0A
-
 # URL d'accès à l'API
 SMSAPI_BASEURL="https://smsapi.free-mobile.fr"
 
 # Action d'envoi de notification
-SMSAPI_SEND_ACTION="sendmsg"
+SMSAPI_SEND_ACTION=sendmsg
 
 
 ##
 ## Configuration utilisateur
 ##
 
-# Login utilisateur / identifiant Free Mobile (celui utilisé pour accéder à l'Espace Abonné)
+# Login utilisateur / identifiant Free Mobile (celui utilisé pour
+# accéder à l'Espace Abonné)
 USER_LOGIN="1234567890"
 
-# Clé d'identification (générée et fournie par Free Mobile via l'Espace Abonné, "Mes Options" : https://mobile.free.fr/moncompte/index.php?page=options)
+# Clé d'identification (générée et fournie par Free Mobile via
+# l'Espace Abonné, "Mes Options" :
+# https://mobile.free.fr/moncompte/index.php?page=options)
 API_KEY="s0me5eCre74p1K3y"
 
 # Texte qui sera ajouté AVANT chaque message envoyé
-MESSAGE_HEADER="Notification :${NEWLINE_CHAR}"
+MESSAGE_HEADER="Notification :
+"
 
 # Texte qui sera ajouté APRÈS chaque message envoyé
-MESSAGE_FOOTER="${NEWLINE_CHAR}--${NEWLINE_CHAR}Le serveur de la maison"
+MESSAGE_FOOTER="
+--
+Le serveur de la maison"
 
 
 ##
 ## Traitement du message
 ##
 
-MESSAGE_TO_SEND=""
-if [ "$1" ]; then # Message en tant qu'argument de la ligne de commande
-    MESSAGE_TO_SEND="$1"
-else # Message lu de STDIN
-    while read line
-    do
-        MESSAGE_TO_SEND="$MESSAGE_TO_SEND$line$NEWLINE_CHAR"
-    done
-    MESSAGE_TO_SEND=$(echo $MESSAGE_TO_SEND | sed 's/'$NEWLINE_CHAR'$//') # Retire le dernier saut de ligne
+if [ "$*" ]; then
+	# Message en tant qu'argument de la ligne de commande
+    MESSAGE_TO_SEND="$*"
+else
+	# Message lu de STDIN
+	MESSAGE_TO_SEND="$(cat)"
 fi
-
-FINAL_MESSAGE_TO_SEND="$MESSAGE_HEADER$MESSAGE_TO_SEND$MESSAGE_FOOTER" # Assemble header, message et footer
+# Assemble header, message et footer
+FINAL_MESSAGE_TO_SEND="$MESSAGE_HEADER$MESSAGE_TO_SEND$MESSAGE_FOOTER"
 
 ##
 ## Appel à l'API (envoi)
@@ -69,15 +69,22 @@ FINAL_MESSAGE_TO_SEND="$MESSAGE_HEADER$MESSAGE_TO_SEND$MESSAGE_FOOTER" # Assembl
 # echo "Will send the following to $USER_LOGIN:" #DEBUG
 # echo "$FINAL_MESSAGE_TO_SEND" #DEBUG
 
-# --insecure : Certificat de $SMSAPI_BASEURL ne fourni pas d'informations sur son propriétaire
-# --write-out "%{http_code}" --silent --output /dev/null : Renvoi le code réponse HTTP uniquement
-HTTP_STATUS_CODE=$(curl --insecure --get "$SMSAPI_BASEURL/$SMSAPI_SEND_ACTION" --data "user=$USER_LOGIN" --data "pass=$API_KEY" --data "msg=$FINAL_MESSAGE_TO_SEND" --write-out "%{http_code}" --silent --output /dev/null)
+# --insecure : Certificat de $SMSAPI_BASEURL ne fourni pas
+# --d'informations sur son propriétaire write-out "%{http_code}"
+# ----silent --output /dev/null : Renvoi le code réponse HTTP
+# --uniquement
+HTTP_STATUS_CODE=$(curl --insecure \
+	--get "$SMSAPI_BASEURL/$SMSAPI_SEND_ACTION" \
+	--data "user=$USER_LOGIN" --data "pass=$API_KEY" \
+	--data-urlencode "msg=$FINAL_MESSAGE_TO_SEND" \
+	--write-out "%{http_code}" --silent --output /dev/null)
 
 # Codes réponse HTTP possibles
 # 200 : Le SMS a été envoyé sur votre mobile.
 # 400 : Un des paramètres obligatoires est manquant.
 # 402 : Trop de SMS ont été envoyés en trop peu de temps.
-# 403 : Le service n'est pas activé sur l'espace abonné, ou login / clé incorrect.
+# 403 : Le service n'est pas activé sur l'espace abonné, 
+#       ou login / clé incorrect(e).
 # 500 : Erreur côté serveur. Veuillez réessayez ultérieurement.
 
 if [ "$HTTP_STATUS_CODE" -eq 200 ]; then
